a307b1d9d9c9e8597c8ba26f34302a27871dbb7b
Colin Finger <colin.finger@pmcs.de>
2020-07-18 14:40:14 +0200

    added storybook

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextboxComponent } from './textbox/textbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextboxComponent
   ],
   imports: [
     BrowserModule,
