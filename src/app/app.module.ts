ada410db9461c22bc52fc6cddc44d54ebb758e8e
David_Alexander25 <david.j.alexander.25@gmail.com>
2020-07-22 23:25:29 +0100

    Revert "more storybook + reduced tsconfig files"
    This reverts commit 4e26821b40ce0e693a8797a5e012bd2c03395f09.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TextboxComponent } from './textbox/textbox.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -17,11 +12,7 @@ import { MatInputModule } from '@angular/material/input';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    MatFormFieldModule,
-    MatInputModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/textbox/textbox.component.ts
+++ src/app/textbox/textbox.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
     selector: 'app-textbox',
@@ -8,8 +6,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     styleUrls: ['./textbox.component.scss']
 })
 export class TextboxComponent implements OnInit {
-    value;
-    autocomplete = 'off';
 
     constructor() { }
 
